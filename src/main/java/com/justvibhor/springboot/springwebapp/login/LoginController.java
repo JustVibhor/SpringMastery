a4b04f2b96ffbdbe8957e682c42bcbe90fb9a7a5
JustVibhor <vibhorebhatneriya@gmail.com>
2023-07-29 21:15:57 +0530

    Login page kind of

--- src/main/java/com/justvibhor/springboot/springwebapp/login/LoginController.java
+++ src/main/java/com/justvibhor/springboot/springwebapp/login/LoginController.java
@@ -1,22 +1,15 @@
 package com.justvibhor.springboot.springwebapp.login;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
 public class LoginController {
 	
-	private Logger logger = LoggerFactory.getLogger(getClass()); 
 	
 	@RequestMapping("/login")
-	public String handleLogin(@RequestParam String name, ModelMap model) {
-		model.put("name", name);
-		logger.info("Request param is {}", name);
+	public String handleLogin() {
 		return "login";
 	}
 }
--- src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
+++ src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
@@ -3,6 +3,11 @@
 		<title>Login</title>
 	</head>
 	<body>
-		Hi, you are logged in ${name}
+		Login Page
+		<form method="post">
+			Name: <input type="text" name="name" >
+			Password: <input type="password" name="password" >
+			<input type="submit">
+		</form>
 	</body>
 </html>
