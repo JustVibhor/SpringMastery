f0d8f67c63a47de935191653d400cdc672f0ad88
JustVibhor <vibhorebhatneriya@gmail.com>
2023-07-29 21:39:42 +0530

    Authentication Service

--- src/main/java/com/justvibhor/springboot/springwebapp/login/LoginController.java
+++ src/main/java/com/justvibhor/springboot/springwebapp/login/LoginController.java
@@ -8,6 +8,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
+	
+	private AuthenticationService authenticationService;
+
+	public LoginController(AuthenticationService authenticationService) {
+		super();
+		this.authenticationService = authenticationService;
+	}
 
 	@RequestMapping(value = "login", method = RequestMethod.GET)
 	public String handleLogin() {
@@ -16,8 +23,13 @@ public class LoginController {
 
 	@RequestMapping(value = "login", method = RequestMethod.POST)
 	public String gotoWelcomePage(@RequestParam String name, @RequestParam String password, ModelMap model) {
-		model.put("name", name);
-		model.put("password", password);
-		return "welcome";
+		
+		if(authenticationService.authenticate(name, password)) {			
+			model.put("name", name);
+			model.put("password", password);
+			return "welcome";
+		}
+		
+		return "login";
 	}
 }
