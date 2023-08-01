adff4248cce472dbab8c963348a8997b3489bf3f
JustVibhor <vibhorebhatneriya@gmail.com>
2023-08-01 13:30:00 +0530

    rendering post data

--- src/main/java/com/justvibhor/springboot/springwebapp/todo/TodoService.java
+++ src/main/java/com/justvibhor/springboot/springwebapp/todo/TodoService.java
@@ -10,16 +10,22 @@ import org.springframework.stereotype.Service;
 public class TodoService {
 
 	private static List<Todo> todos = new ArrayList<>();
+	private static int todoCount = 0;
 
 	static {
-		todos.add(new Todo(1, "JustVibhor", "Learn AWS", LocalDate.now().plusYears(1), false));
-		todos.add(new Todo(2, "JustVibhor", "DevOps", LocalDate.now().plusYears(1), false));
-		todos.add(new Todo(3, "JustVibhor", "Qubits", LocalDate.now().plusYears(3), false));
+		todos.add(new Todo(++todoCount, "JustVibhor", "Learn AWS", LocalDate.now().plusYears(1), false));
+		todos.add(new Todo(++todoCount, "JustVibhor", "DevOps", LocalDate.now().plusYears(1), false));
+		todos.add(new Todo(++todoCount, "JustVibhor", "Qubits", LocalDate.now().plusYears(3), false));
 	}
 
 	public List<Todo> findByUsername(String userName) {
 
 		return todos;
+	}
+
+	public void addTodo(String userName, String description, LocalDate targetDate, boolean isDone) {
+		Todo todo = new Todo(++todoCount, userName, description, targetDate, isDone);
+		todos.add(todo);
 	}
 
 }
