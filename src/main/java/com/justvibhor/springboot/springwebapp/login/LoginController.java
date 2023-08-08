e7b56bc87f0c48b4bcb8fdbef1911393c3e34926
JustVibhor <vibhorebhatneriya@gmail.com>
2023-08-08 14:54:52 +0530

    Remove excess login functionality

--- src/main/java/com/justvibhor/springboot/springwebapp/login/LoginController.java
+++ src/main/java/com/justvibhor/springboot/springwebapp/login/LoginController.java
@@ -4,34 +4,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
 @SessionAttributes("name")
 public class LoginController {
-	
-	private AuthenticationService authenticationService;
 
-	public LoginController(AuthenticationService authenticationService) {
-		super();
-		this.authenticationService = authenticationService;
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String handleLogin(ModelMap model) {
+		model.put("name", "JustVibhor");
+		return "welcome";
 	}
 
-	@RequestMapping(value = "login", method = RequestMethod.GET)
-	public String handleLogin() {
-		return "login";
-	}
-
-	@RequestMapping(value = "login", method = RequestMethod.POST)
-	public String gotoWelcomePage(@RequestParam String name, @RequestParam String password, ModelMap model) {
-		
-		if(authenticationService.authenticate(name, password)) {			
-			model.put("name", name);
-			model.put("password", password);
-			return "welcome";
-		}
-		model.put("errorMessage", "Bhai Username aur password galat heh");
-		return "login";
-	}
 }
