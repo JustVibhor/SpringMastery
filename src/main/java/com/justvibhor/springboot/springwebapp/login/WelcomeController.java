35fd521978ceedc3257f7a5aefe84848691062dd
JustVibhor <vibhorebhatneriya@gmail.com>
2023-08-08 22:30:12 +0530

    Spring-security

--- src/main/java/com/justvibhor/springboot/springwebapp/login/WelcomeController.java
+++ src/main/java/com/justvibhor/springboot/springwebapp/login/WelcomeController.java
@@ -8,10 +8,10 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
 @SessionAttributes("name")
-public class LoginController {
+public class WelcomeController {
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String handleLogin(ModelMap model) {
+	public String goToWelcomePage(ModelMap model) {
 		model.put("name", "JustVibhor");
 		return "welcome";
 	}
