2ffaf8bf8661299b87e60b0b75bceb7b76117bd5
JustVibhor <vibhorebhatneriya@gmail.com>
2023-08-11 02:14:24 +0530

    Handling h2 console whitelable error

--- src/main/java/com/justvibhor/springboot/springwebapp/security/SpringSecurityConfiguration.java
+++ src/main/java/com/justvibhor/springboot/springwebapp/security/SpringSecurityConfiguration.java
@@ -1,43 +1,54 @@
 package com.justvibhor.springboot.springwebapp.security;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 import java.util.function.Function;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SpringSecurityConfiguration {
-	
+
 	// manages user username, password and role
 	@Bean
 	public InMemoryUserDetailsManager createUserDetailManager() {
-		
+
 		UserDetails userDetail1 = createNewUser("JustVibhor", "1234");
 		UserDetails userDetail2 = createNewUser("RahulFool", "4321");
-		
+
 		return new InMemoryUserDetailsManager(userDetail1, userDetail2);
 	}
 
 	private UserDetails createNewUser(String username, String password) {
-		Function<String, String> passwordEncoder
-		= input -> passwordEncoder().encode(input);
-		UserDetails userDetails = User.builder()
-									.passwordEncoder(passwordEncoder)
-									.username(username)
-									.password(password)
-									.roles("USER", "ADMIN")
-									.build();
+		Function<String, String> passwordEncoder = input -> passwordEncoder().encode(input);
+		UserDetails userDetails = User.builder().passwordEncoder(passwordEncoder).username(username).password(password)
+				.roles("USER", "ADMIN").build();
 		return userDetails;
+		
 	}
-	
+
 	// does salting and all
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated());
+		http.formLogin(withDefaults());
+		
+		http.csrf().disable();
+		http.headers().frameOptions().disable();
+		
+		return http.build();
 	}
 }
